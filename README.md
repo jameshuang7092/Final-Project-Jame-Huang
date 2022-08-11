b2bfaab8ac9193d36d5099a50c32c12e1dcb8603
jameshuang7092 <108740253+jameshuang7092@users.noreply.github.com>
2022-08-10 20:04:10 -0400

    transferred from google colab

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Final Project-Jame Huang
- final project
+
+# Final Project - Jame Huang
+ 
+ 
+ This is my final project for TECH 2335 Summer 2022.
+
+
+I create a program that informs a budding trader whether there would be an arbitrage oppourtunity in the global crypto markets, for example, the price of Bitcoin may be $10,000 in one region and $15,000 in another.
+
+Due to the extreme volalitlity of the nascent crypto market, the oppourtunity will be risk-adjusted based on the traders' risk tolerance (which may be arbitrary).
+
+In attempting to connect via API with overseas crypto centralized exchanges, I learned that all of them required either a premium subscription (not free) or required enhanced verificiation (required me to uploaded a picture of myself and an ID), which I was not willing to do.
+
+To get around this, for demostration purposes, I decided to pick a specific market (South Korea), and downloadeded a csv file listing daily prices of BTC in South Korea from: https://www.investing.com/crypto/bitcoin/btc-krw-historical-data; I then converted the Korean won (KRW) currency back to USD by multiplying each daily close with the daily exchange rate from: https://www.investing.com/crypto/bitcoin/btc-krw-historical-data. (I could not utilize alphavantage's exchange rate because it did not have historical values).
+
+
+Step 1:
+Install Chart Studio with "$pip install chart_studio" via the terminal.
+
+Step 2:
+$python program.py
+
+Step 3:
+Check historical arbitrage opportunities with markets outside of the USA by inputting crypto symbol (for demo purposes, please use BTC). We will be using the USA market as the benchmark.
+
+Step 4: 
+Input the market outside of the USA to view historical arbitrage opportunities (for demo purposes, please use KRW).
+
+
+
+
---
+++ program.py
@@ -0,0 +1,74 @@
+import requests
+from getpass import getpass
+from pandas import read_csv
+from numpy.ma.core import var # https://stackoverflow.com/questions/9390126/pythonic-way-to-check-if-something-exists
+import plotly.graph_objects as go
+import pandas as pd
+#!pip install chart_studio
+import chart_studio
+import chart_studio.plotly as py
+import chart_studio.presentation_objs as pres
+
+symbol = input("Please input crypto ticker: ").upper()
+api_key = input("Enter API key for Alphavantage: ")
+market = input("Please input market outside the USA: ").upper()
+csv_url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={symbol}&market=USD&apikey={api_key}&datatype=csv"
+
+
+df_local_upload = pd.read_csv (r'C:\Users\jamee\OneDrive\Desktop\plotly_final_1.csv')
+
+df_alphavantage = pd.read_csv(f'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={symbol}&market=USD&apikey={api_key}&datatype=csv')
+
+fig = go.Figure([go.Scatter(x=df_alphavantage['timestamp'], y=df_alphavantage['close (USD)'])])
+fig.update_layout(title='Daily Crypto Prices in USA',
+                   xaxis_title='Month',
+                   yaxis_title=f'{symbol}')
+fig.update_layout(yaxis_tickprefix = '$', yaxis_tickformat = ',.2f')
+fig.show()
+
+df_uploaded = pd.DataFrame(list(df_local_upload.items()), columns = ['timestamp','BTC price in SK'])
+df_uploaded_csv = pd.read_csv(r'C:\Users\jamee\OneDrive\Desktop\plotly_final_1.csv')
+
+fig = go.Figure([go.Scatter(x=df_uploaded_csv['Date'], y=df_uploaded_csv['BTC price in SK'])])
+fig.update_layout(title='Daily Crypto Prices in non-US market',
+                   xaxis_title='Month',
+                   yaxis_title=f'{symbol}')
+fig.update_layout(yaxis_tickprefix = '$', yaxis_tickformat = ',.2f')
+fig.show()
+
+trace0 = go.Scatter(
+    x=df_uploaded_csv['Date'],
+    y=df_uploaded_csv['close (USD)']
+)
+trace1 = go.Scatter(
+    x=df_uploaded_csv['Date'],
+    y=df_uploaded_csv['BTC price in SK']
+)
+data = [trace0, trace1]
+layout=go.Layout(barmode='stack')
+fig.update_layout(title='US vs. non-US market',
+                   xaxis_title='Month',
+                   yaxis_title=f'{symbol}')
+fig=go.Figure(data=data, layout=layout)
+
+fig.show()
+py.iplot(data, filename = 'test')
+
+def to_pct(my_number):
+    return f"{(my_number * 100):.4f}%"
+
+print("Today's date is: ")
+print(df_uploaded_csv['Date'][998])
+print("The daily market-close difference between markets is: ")
+percent_change = to_pct((df_uploaded_csv['BTC price in SK'][998]-df_uploaded_csv['close (USD)'][998])/df_uploaded_csv['close (USD)'][998])
+print(percent_change)
+
+to_float1 = percent_change.strip('%')
+to_float2 = float(to_float1)
+
+if to_float2 >= 5:
+    print("Low-risk price arbitrage possible")
+else:
+    print("Not worth the risk")
+
+
